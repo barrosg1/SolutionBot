c599013e2e1f5bbbe7eb47bfa2430a758d485cc8
kit <mcgabe11@gmail.com>
2016-12-29 23:56:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+SolutionBot application
+
+This app is served to answer user's questions from the input and retrieve date from wolframalpha api and wikipedia.
+
+To run this program simply type:
+
+- python run.py
+
---
+++ api.py
@@ -0,0 +1,64 @@
+import time
+import datetime
+
+greetings = {
+	"how are you":"I'm fine, thank you.",
+	"good morning":"Morning",
+	"good afternoon":"Good afternoon",
+	"good evening":"Good evening",
+	"good night":"Good night",
+	"how is your day":"My day is always good!",
+	"how are you":"I'm fine, thank you.",
+	"how r you":"I'm fine, thank you.",
+	"how r u":"I'm fine, thank you.",
+	"what is your name":"my name is solution bot.",
+	"how old are you":"Too old for you.",
+	"how old r u":"Too old for you.",
+	"what's up":"Nothing much. You?",
+	"whats up":"Nothing much. You?",
+	"sup":"Nothing much. You?",
+	"what's good":"Nothing much. You?",
+	"whats good":"Nothing much. You?",
+	"hows it going":"Nothing much. You?",
+	"how's it going":"Nothing much. You?",
+	"what's popping":"Nothing much. You?",
+	"wassup":"Nothing much. You?",
+
+}
+
+compliment = {
+	"youre awesome":"Thank you, so are you!",
+	"you're awesome":"Thank you, so are you!",
+	"i like you":"i like you too!",
+	"i love you":"i love you too!",
+	"youre handsome":"Thanks!",
+	"you're handsome":"Thanks!",
+	"youre pretty":"Thanks!",
+	"you're pretty":"Thanks!",
+	"youre beautiful":"Thanks!",
+	"you're beautiful":"Thanks!",
+	"youre amazing":"Thanks!",
+	"you're amazing":"Thanks!",
+	"you're smart":"Thanks!",
+	"you're useful":"Thanks!",
+	"you're intelligence":"Thanks!",
+}
+
+profanities = {
+	"shut up":"what have I done to you?",
+	"stfu":"what have I done to you?",
+	"youre ugly":"That's your opinion",
+	"you're ugly":"That's your opinion",
+	"motherfucker":"No need for profanities.",
+	"fuck you":"No need for profanities.",
+	"fu":"No need for profanities.",
+	"stupid":"If that's what you think...",
+	"youre stupid":"If that's what you think...",
+	"you're stupid":"If that's what you think...",
+	"idiot":"Sorry, if I offended you",
+	"you idiot":"Sorry, if I offended you",
+	"u idiot":"Sorry, if I offended you",
+	"dumbass":"what have I done to you?",
+	"dumb":"what have I done to you?",
+}
+
